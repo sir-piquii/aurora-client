feb65efcd2a7e1f72dab8659ded9c31e6b0c0d68
Pedzainhamo Mavedzenge <pedzi@zhdconsulting.com>
2025-02-06 21:26:31 +0200

    fix product columns

--- src/components/ProductsComponent.js
+++ src/components/ProductsComponent.js
@@ -53,7 +53,7 @@ export default function ProductSection() {
 			<h2 className="text-3xl font-bold text-center mb-8">
 				Explore Our Products
 			</h2>
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 				{products.map((product, index) => (
 					<Link
 						to={product.link}
