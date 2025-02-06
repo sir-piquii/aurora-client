169a924c13765e3eec3a4c447272436e6cea4d13
Gideon Mavedzenge <gideonmavedzenge@Gideons-MacBook-Pro.local>
2025-02-06 23:45:12 +0000

    Fix footer cards

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -51,27 +51,25 @@ export default function Footer() {
 					{/* Cards container spanning 10 columns on medium/large screens */}
 					<div className="md:col-span-10">
 						{/* Inner grid for cards: 1 column on mobile, 3 columns on medium and up */}
-						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6">
 							{/* WhatsApp Card */}
 							<a
-								className="p-8 bg-black/40 rounded-lg shadow-lg text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl"
+								className="p-8 bg-black/40 rounded-lg shadow-lg flex flex-col items-center justify-center text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl"
 								href="https://wa.me/263771683662"
 								target="_blank"
 								rel="noopener noreferrer"
 							>
-								<div>
-									<FaWhatsapp
-										size={40}
-										className="mx-auto text-white mb-3 transition-all duration-300 hover:text-orange-500"
-									/>
-									<p className="text-lg md:text-base lg:text-lg">
-										+263 771 683 662
-									</p>
-								</div>
+								<FaWhatsapp
+									size={40}
+									className="mx-auto text-white mb-3 transition-all duration-300 hover:text-orange-500"
+								/>
+								<p className="text-lg md:text-base lg:text-lg">
+									+263 771 683 662
+								</p>
 							</a>
 
 							{/* Email Card */}
-							<div className="p-8 bg-black/40 rounded-lg shadow-lg text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl">
+							<div className="p-8 bg-black/40 rounded-lg shadow-lg flex flex-col items-center justify-center text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl">
 								<FaEnvelope
 									size={40}
 									className="mx-auto text-white mb-3 transition-all duration-300 hover:text-orange-500"
@@ -85,7 +83,6 @@ export default function Footer() {
 										info@auroraenergy.co.zw
 									</a>
 								</p>
-
 								<p>
 									<a
 										href="mailto:sales@auroraenergy.co.zw"
@@ -98,7 +95,7 @@ export default function Footer() {
 							</div>
 
 							{/* Address Card */}
-							<div className="p-8 bg-black/40 rounded-lg shadow-lg text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl">
+							<div className="p-8 bg-black/40 rounded-lg shadow-lg flex flex-col items-center justify-center text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl">
 								<FaMapPin
 									size={40}
 									className="mx-auto text-white mb-3 transition-all duration-300 hover:text-orange-500"
@@ -110,7 +107,7 @@ export default function Footer() {
 							</div>
 
 							{/* Google Maps Embed Card */}
-							<div className="bg-black/40 rounded-lg shadow-lg text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl">
+							<div className="bg-black/40 rounded-lg shadow-lg flex flex-col items-center justify-center text-center transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-xl">
 								<iframe
 									src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3799.0558102848736!2d31.060250176397574!3d-17.789073975229652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1931a544a54048a5%3A0xbdd15d3dd3ecc8d3!2s1%20College%20Rd%2C%20Harare%2C%20Zimbabwe!5e0!3m2!1sen!2suk!4v1738663720400!5m2!1sen!2suk"
 									width="100%"
@@ -131,10 +128,6 @@ export default function Footer() {
 
 					{/* Social Links Sidebar spanning 2 columns on medium/large screens */}
 					<div className="md:col-span-2 flex items-center justify-center">
-						{/* 
-              On mobile, display links in a row with space between them.
-              On medium and up, display links in a column.
-            */}
 						<div className="flex flex-row space-x-4 md:flex-col md:space-y-4 md:space-x-0">
 							<a
 								href="https://www.facebook.com/share/15imcziZ2b/"
